7a82f3a19eb7ed25ac15f64e2c89f4874acdb206
Eliott Nys <eliott.nys@gmail.com>
2023-05-11 15:06:11 +0200

    added obstacle management to ino file

--- examples/final_ros/final_ros.ino
+++ examples/final_ros/final_ros.ino
@@ -3,8 +3,11 @@
 #include <ros.h>
 #include <std_msgs/UInt16.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/String.h>
 
 #include <Motors.h>
+
+#include <NewPing.h>
 
 ros::NodeHandle nh;
 
@@ -15,6 +18,15 @@ const unsigned int pin_IN1 = 7;
 const unsigned int pin_IN2 = 8;
 const unsigned int pin_IN3 = 9;
 const unsigned int pin_IN4 = 10;
+
+#define TRIGGER_PIN  4
+#define ECHO_PIN     5
+#define MAX_DISTANCE 2000
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+std_msgs::String mode_msg;
+int low_anti_bounce = 5;
+int high_anti_bounce = 5;
+int cycle_divide = 50;  // Wait 50ms between pings (recommended by NewPing library)
 
 //initialize motors
 Motors motors(pin_ENA, pin_ENB, pin_IN1, pin_IN2, pin_IN3, pin_IN4);
@@ -53,6 +65,7 @@ ros::Subscriber<std_msgs::Float64> left_speed_subscriber("left_speed", left_spee
 ros::Subscriber<std_msgs::Float64> right_speed_subscriber("right_speed", right_speed_callback);
 ros::Subscriber<std_msgs::UInt16> left_direction_subscriber("left_direction", left_direction_callback);
 ros::Subscriber<std_msgs::UInt16> right_direction_subscriber("right_direction", right_direction_callback);
+ros::Publisher mode_publisher("mode", &mode_msg);
 
 void setup() {
   nh.initNode();
@@ -60,6 +73,7 @@ void setup() {
   nh.subscribe(right_speed_subscriber);
   nh.subscribe(left_direction_subscriber);
   nh.subscribe(right_direction_subscriber);
+  nh.advertise(mode_publisher);
   motors.setLeftToForward();
   motors.setRightToForward();
   motors.setLeftSpeed(0);
@@ -67,6 +81,27 @@ void setup() {
 }
 
 void loop() {
+  cycle_divide = cycle_divide - 1;
+  if (cycle_divide == 0){
+    int dist = sonar.ping_cm();
+    if (dist < 12 && dist != 0){
+      high_anti_bounce = 5;
+      low_anti_bounce = low_anti_bounce - 1;
+    }
+    else {
+      low_anti_bounce = 5;
+      high_anti_bounce = high_anti_bounce - 1;
+    }
+    if (low_anti_bounce == 0){
+      mode_msg.data = "off";
+      mode_publisher.publish(&mode_msg);
+    }
+    else if (high_anti_bounce == 0){
+      mode_msg.data = "auto";
+      mode_publisher.publish(&mode_msg);
+    }
+    cycle_divide = 75;
+  }
   nh.spinOnce();
   delay(1);
 }
